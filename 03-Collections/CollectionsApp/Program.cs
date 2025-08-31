60c9eff3f4a70cd4f309293f6da667f8fd089342
ArdaSalginci <ardasalg@gmail.com>
2025-09-01 02:23:28 +0300

    fix: latest changes

--- 03-Collections/CollectionsApp/Program.cs
+++ 03-Collections/CollectionsApp/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+
+namespace collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
