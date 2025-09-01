778d3f8d701050dfa6407495806908deb1228931
ArdaSalginci <ardasalg@gmail.com>
2025-09-01 03:58:37 +0300

    feat: update collections example with ArrayList usage

--- 03-Collections/CollectionsApp/Program.cs
+++ 03-Collections/CollectionsApp/Program.cs
@@ -1,4 +1,8 @@
 ﻿
+using System.Collections;
+using System.Linq.Expressions;
+using System.Net.Sockets;
+
 namespace collections
 {
     class Program
@@ -10,13 +14,34 @@ namespace collections
             // =>ArrayList => int, string 
             // => genetic collections 
             // => int, string, object
-   
+
+            ArrayList liste = new ArrayList();
+            liste.Add(10);
+            liste.Add("10");
+            liste.Add("arda");
+            liste.Add(null);
+            liste.Add(true);
+            var liste2 = new ArrayList()
+            {
+                5,
+                "ahmet",
+                false,
+                4,5,
+                null
+            };
+            int[] sayilar = { 10, 20, 30 };
+            liste.AddRange(sayilar);
+            var eleman = (int)liste[0];
+            var isim = liste[2];
+            Console.WriteLine(eleman);
+            Console.WriteLine(isim); 
+       
 
 
 
-    
+        }
         }
     }
-}
+
 
 
